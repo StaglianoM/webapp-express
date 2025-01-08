e8176a116d1691269a4043173bd152e7f9300306
StaglianoM <stagliano.m92@gmail.com>
2025-01-08 17:43:42 +0100

    ADD index & show + errors

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,17 +1,54 @@
 const connection = require('../data/db')
 
+
+
 //elenco dei film
 function index(req, res) {
-    res.json({
-        message: 'index movies'
+    const sql = `SELECT * FROM movies`
+
+
+    connection.query(sql, (err, movies) => {
+        // console.log(err)
+        if (err) return res.status(500).json({ message: err.message })
+        // console.log(movies)
+
+        res.json({
+            message: 'index movies'
+        })
+
     })
 }
 
+
+
 //elenco del singolo film e recensione
 function show(req, res) {
-    res.json({
-        message: 'show movies'
-    })
+    const id = parseInt(req.params.id);
+
+    if (isNaN(id)) {
+        return res.status(400).json({
+            error: 'ID non valido'
+        });
+    }
+
+    const sql = `SELECT * FROM movies WHERE id = ?`;
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) {
+            console.error('Errore durante la query:', err);
+            return res.status(500).json({
+                error: 'Errore del server'
+            });
+        }
+
+        if (results.length === 0) {
+            return res.status(404).json({
+                message: 'Film non trovato'
+            });
+        }
+    });
+    // Restituisce il primo risultato come oggetto JSON
+    res.json(results[0]);
 }
 
 
