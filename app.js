b0c1e591763b29d84c8d0f5121fc3bec1db1bc8e
StaglianoM <stagliano.m92@gmail.com>
2025-01-08 16:07:05 +0100

    add get

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Test')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+//rotta get di root
+app.get('/', (req, res) => {
+    res.send('server connesso')
+})
+
+//connessione alla port 3000
+app.listen(port, () => {
+    console.log(`server connesso port ${port}`)
+})
